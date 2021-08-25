c56d6b45adc631c769598c8b0e023bd286e5feea
Quinn Slack <quinn@slack.org>
2021-08-24 23:45:04 -0700

    wip

--- client/render-web/babel.config.js
+++ client/render-web/babel.config.js
@@ -3,6 +3,31 @@
 /** @type {import('@babel/core').TransformOptions} */
 const config = {
   extends: '../../babel.config.js',
+   "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "modules": "auto",
+        "targets": {
+          "node": "current"
+        }
+      }
+    ],
+    "@babel/preset-react",
+    "@babel/preset-typescript"
+  ],
+  "plugins": [
+    ["transform-import-css", {
+      // TODO(sqs): sync up with webpack.config.js localIdentName
+      "generateScopedName": "[name]__[local]_[hash:base64:5]"
+    }],
+    [
+      "babel-plugin-transform-require-ignore",
+      {
+        "extensions": [".scss"]
+      }
+    ]
+  ]
 }
 
 module.exports = config
