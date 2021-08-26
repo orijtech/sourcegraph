a2f70cb56057b56245dc89b2c2ae724ad7f1aead
Quinn Slack <quinn@slack.org>
2021-08-25 22:11:59 -0700

    wip

--- client/render-web/src/serve.ts
+++ client/render-web/src/serve.ts
@@ -6,53 +6,30 @@ import path from 'path'
 
 import { handleRequest } from './handle'
 
-const webpackHost = 'sourcegraph.test'
-const webpackPort = 3080
-
-const port = process.env.PORT || 3079
+const port = process.env.PORT || 3190
 
 http.createServer(async (request, response) => {
-    const PRERENDER = true
-    if (PRERENDER && (request.url! === '/' || request.url! === '/search')) {
-        const SPAWN = false
-        if (SPAWN) {
-            const child = child_process.execFile('babel-node', [
-                '-x',
-                '.tsx,.ts',
-                path.join(__dirname, 'handle.tsx'),
-                request.url!,
-            ])
-            response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-            child.stdout!.pipe(response)
-            child.on('error', error => console.error(error))
-            return
-        }
-
+    const SPAWN = false
+    if (SPAWN) {
+        const child = child_process.execFile('babel-node', [
+            '-x',
+            '.tsx,.ts',
+            path.join(__dirname, 'handle.tsx'),
+            request.url!,
+        ])
         response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-        try {
-            await handleRequest(response, request.url!, require('./jscontext').JSCONTEXT, {})
-        } catch (err) {
-            console.error('ERROR', err)
-        }
+        child.stdout!.pipe(response)
+        child.on('error', error => console.error(error))
         return
     }
 
-    const options: http.RequestOptions = {
-        hostname: webpackHost,
-        port: webpackPort,
-        path: request.url!,
-        method: request.method,
-        headers: request.headers,
+    // TODO(sqs): handle when not HTTP 200 (support other HTTP status codes, including redirects)
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    try {
+        await handleRequest(response, request.url!, require('./jscontext').JSCONTEXT, {})
+    } catch (err) {
+        console.error(`Error (${request.url}): ${err}`)
     }
-
-    // Forward each incoming request
-    const proxyRequest = http.request(options, async proxyRes => {
-        response.writeHead(proxyRes.statusCode!, proxyRes.headers)
-        proxyRes.pipe(response, { end: true })
-    })
-
-    // Forward the body of the request to esbuild
-    request.pipe(proxyRequest, { end: true })
 }).listen(port)
 
-console.error(`Ready at http://localhost:${port}`)
+console.error(`Ready at: http://localhost:${port}`)
